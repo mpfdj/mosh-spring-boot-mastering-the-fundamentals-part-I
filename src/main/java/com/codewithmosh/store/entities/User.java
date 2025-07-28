1350626a04e640ff8f9fbe67798a367078ce65ec
Miel de Jaeger <miel.de.jaeger@ing.com>
2025-07-28 14:30:39 +0200

    Add wishlist

--- src/main/java/com/codewithmosh/store/entities/User.java
+++ src/main/java/com/codewithmosh/store/entities/User.java
@@ -58,6 +58,13 @@ public class User {
     private Profile profile;
 
 
+    @ManyToMany
+    @JoinTable(
+            name = "wishlist",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "product_id")
+    )
+    private Set<Product> favoriteProducts = new HashSet<>();
 
 
 }
